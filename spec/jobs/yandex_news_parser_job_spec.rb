3356d0045b6462724d9d43549f08afecb7743ee0
Andrey Nikulshin <anikulshin@Andreys-MBP.Dlink>
2017-11-21 17:44:06 +0500

    Add rspec YandexNewsParserJob#perform

--- spec/jobs/yandex_news_parser_job_spec.rb
+++ spec/jobs/yandex_news_parser_job_spec.rb
@@ -1,15 +1,44 @@
 require "rails_helper"
 
 describe YandexNewsParserJob do
-  subject(:job) { described_class.perform_later(key) }
-
   let(:key) { 123 }
+  subject(:job) { described_class.perform_later(key) }
+  subject(:job_now) { described_class.perform_now }
 
-  it 'queues the job' do
-    ActiveJob::Base.queue_adapter = :test
+  describe "queues" do
+    it 'queues the job' do
+      ActiveJob::Base.queue_adapter = :test
 
-    expect { job }.to have_enqueued_job(described_class)
-      .with(key)
-      .on_queue("yandex_news")
+      expect { job }.to have_enqueued_job(described_class)
+        .with(key)
+        .on_queue("yandex_news")
+    end
+  end
+
+  describe "#perform" do
+    before(:each) do
+      ActiveJob::Base.queue_adapter = :test
+      stub_const("YandexNewsParserJob::RSS_URL", 'spec/files/yandex_news.rss')
+    end
+
+    context "when new parsed news from yandex not yet saved" do
+      it "should add new job to queue" do
+        expect do
+          job_now
+        end.to change(Article, :count).by(1)
+      end
+    end
+
+    context "when new parsed news from yandex yet saved" do
+      before(:each) do
+        job_now
+      end
+
+      it "should not add new job to queue", focus: true do
+        expect do
+          job_now
+        end.to change(Article, :count).by(0)
+      end
+    end
   end
 end
